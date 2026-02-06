52e0d5e1c06a0565ef3c6d8dcaf89613e3c52cf6
Pietro <pietro.g.souza@aluno.senai.br>
2026-02-06 16:32:46 -0300

    Atualizando a host

--- app.py
+++ app.py
@@ -40,6 +40,6 @@ def adicionar_comentario():
     print(lista_de_comentario)
     return redirect("/comentario")
 
-
-app.run(debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0",port=8080)
 
